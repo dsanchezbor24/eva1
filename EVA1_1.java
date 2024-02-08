8c78009b2650d6f809b849f190e5aab5c5f72307
dsanchezbor24 <159486460+dsanchezbor24@users.noreply.github.com>
2024-02-08 14:21:13 -0600

    Add files via upload

---
+++ EVA1_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1;
+
+/**
+ *
+ * @author sanch
+ */
+public class EVA1_1 {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!");
+    }
+}
